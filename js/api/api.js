cdfbef042e9c5a5157e4665d918594ac9fc0fb5c
champbvt <champ51951@gmail.com>
2020-06-08 23:53:02 +0700

    add api and catch error

--- js/api/api.js
+++ js/api/api.js
@@ -1,7 +1,16 @@
 const API_URL = 'http://localhost:8080/heroes';
-export const login = async () => {
-    const res = await fetch(`${API_URL}`, {
 
+export const login = async (email , pwd) => {
+    const data = {
+        email,
+        pwd
+    }
+    const res = await fetch(`${API_URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
     })
     return res.json()
 }
--- js/login.js
+++ js/login.js
@@ -1,4 +1,27 @@
-$( document ).ready(()=> {
+import {login} from "./api/api.js";
 
+$(document).ready(() => {
+    const $form = $("#login");
+    const $button = $("#signin");
+
+    $form.submit((e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if ($form[0].checkValidity() === true) {
+            $button.prop("disabled",true);
+            login($("#email").val(), $("#password").val()).then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                } else {
+
+                }
+                $button.prop("disabled",false);
+            }).catch(()=>{
+                console.log("There is an error.");
+                $button.prop("disabled",false);
+            })
+        }
+        $form[0].classList.add('was-validated');
+    });
 });
 
