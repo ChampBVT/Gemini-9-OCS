bd2a54736e7fdcf5302d5d8ccc5f84e11c7977e5
champbvt <champ51951@gmail.com>
2020-06-08 22:54:36 +0700

    change login page picture

--- js/api/api.js
+++ js/api/api.js
@@ -1,5 +1,7 @@
 const API_URL = 'http://localhost:8080/heroes';
-export const getTest = async () => {
-    const res = await fetch(`${API_URL}`)
+export const login = async () => {
+    const res = await fetch(`${API_URL}`, {
+
+    })
     return res.json()
 }
